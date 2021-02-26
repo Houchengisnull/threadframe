9d7dc2c3fbdefd82c0f13bc7f31546a944e0a824
Houchengisnull <609937364@qq.com>
2021-02-26 10:23:13 +0800

    对侧边栏添加点击事件 EventHandler

--- src/main/java/com/houc/controller/RootController.java
+++ src/main/java/com/houc/controller/RootController.java
@@ -3,6 +3,7 @@ package com.houc.controller;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,15 +11,18 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.util.Callback;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -34,18 +38,28 @@ public class RootController implements Initializable {
         log.debug("初始化 RootController");
     }
 
+    private HashMap<String, String> sidebarItems = new HashMap<>();
+    private String sidebarPath = "/sidebar.properties";
+    private EventHandler toggleSidebarHandler = new ToggleHandle();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Properties properties = new Properties();
         FileReader in = null;
         try {
-            in = new FileReader(getClass().getResource("/sidebar.properties").getFile());
+            in = new FileReader(getClass().getResource(sidebarPath).getFile());
             properties.load(in);
             Set<Object> keys = properties.keySet();
             for (Object key : keys) {
                 String keyValue = (String) key;
                 String substring = keyValue.substring(keyValue.lastIndexOf(".") + 1);
-                sidebar.getItems().add(new Label(substring));
+                // 存放侧边栏及其对应fxml
+                String fxmlName = properties.getProperty((String) key);
+                sidebarItems.put(substring, fxmlName);
+                // 加载侧边栏
+                Label label = new Label(substring);
+                label.setOnMouseClicked(toggleSidebarHandler);
+                sidebar.getItems().add(label);
             }
         } catch (FileNotFoundException e) {
             log.error(e.getMessage());
@@ -54,21 +68,28 @@ public class RootController implements Initializable {
         }
     }
 
-    static boolean a = true;
-
     @FXML
     public void click(Event event) throws IOException {
-        Pane load = null;
-        if (a) {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/ReadWriteReentrantLock.fxml"));
-            load = (Pane) fxmlLoader.load();
-        } else {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/helloworld.fxml"));
-            load= (Pane) fxmlLoader.load();
-        }
+        Label source = (Label) event.getSource();
+        String sidebarItem = source.getText();
+        String fxmlName = sidebarItems.get(sidebarItem);
+        String fxmlPath = "/fxml/" + fxmlName + ".fxml";
+        log.debug("click sidebar [{}], then change to [{}] pane", sidebarItem, fxmlName);
+        log.debug("fxml path : {}", fxmlPath);
 
-        a = !a;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+        Pane load = (Pane) loader.load();
+
         root.setCenter(load);
+    }
+
+    class ToggleHandle implements EventHandler {
+
+        @SneakyThrows
+        @Override
+        public void handle(Event event) {
+            click(event);
+        }
     }
 
 }
